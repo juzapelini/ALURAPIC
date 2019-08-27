e270e1115bd9ee3745417d7c970688b60e4764dd
Zap <juliano.batista@bradesco.com.br>
2019-08-27 00:01:35 -0300

    fim do curso angular 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,7 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsComponent } from './errors/errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
-import { HomeModule } from './home/home.module';
-import { VmessageComponent } from './shared/components/vmessage/vmessage.component';
-import { HeaderComponent } from './core/header/header.component';
 import { CoreModule } from './core/core.module';
-//import { CardComponent } from './shared/components/card/card.component';
-//import { CardModule } from './shared/components/card/card.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +18,6 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
-    HomeModule,
     CoreModule
   ],
   providers: [],
